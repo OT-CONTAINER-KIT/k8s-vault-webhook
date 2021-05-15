e4930eb417afda14477ee248eda7684d286bad6a
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2021-05-15 20:40:55 +0530

    Added scripts for release
    Signed-off-by: iamabhishek-dubey <abhishekbhardwaj510@gmail.com>

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 build_docs() {
-    cd docs; yarn install
-    cd docs; npm install -D vuepress
-    cd docs; yarn build
+    cd docs; yarn install && \
+        yarn add -D vuepress && \
+        yarn build
 }
 
 build_docs
